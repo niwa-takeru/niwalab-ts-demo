e2012a4ab84987bc139058d2a1acb79aa4cc5a98
tkrplus <niwa.buffer@gmail.com>
2019-08-04 22:38:54 +0900

    refactor TodosComponent

--- src/components/pages/Todos/Todos.tsx
+++ src/components/pages/Todos/Todos.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 import DefaultTemplate from "~/src/components/templates/DefaultTemplate";
 
-interface Props {
+interface TodosProps {
   todos: string[];
   add: (text: string) => void;
   remove: (index: number) => void;
 }
 
-const Todos: React.FC<Props> = props => {
+const Todos: React.FC<TodosProps> = props => {
   const textInput = React.createRef<HTMLInputElement>();
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
@@ -23,21 +23,29 @@ const Todos: React.FC<Props> = props => {
       </form>
       <div>
         {props.todos.map((todo, index) => (
-          <div key={index}>
-            {todo}
-            <button
-              value={index}
-              onClick={e => {
-                e.preventDefault();
-                props.remove(index);
-              }}
-            >
-              remove
-            </button>
-          </div>
+          <Todo key={index} todo={todo} index={index} remove={props.remove} />
         ))}
       </div>
     </DefaultTemplate>
+  );
+};
+
+interface TodoProps {
+  todo: string;
+  index: number;
+  remove: (index: number) => void;
+}
+
+const Todo: React.FC<TodoProps> = props => {
+  const handleRemove = (e: React.MouseEvent) => {
+    e.preventDefault();
+    props.remove(props.index);
+  };
+  return (
+    <div>
+      <button onClick={handleRemove}>remove</button>
+      {props.todo}
+    </div>
   );
 };
 
