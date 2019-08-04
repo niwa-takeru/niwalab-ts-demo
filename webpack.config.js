32f58a00ca41c05752d99c9a35735acf56bd3e03
tkrplus <niwa.buffer@gmail.com>
2019-08-04 22:16:00 +0900

    add production build settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,66 @@
-const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-module.exports = {
+module.exports = (env, options) => {
+  var setting = {
+    mode: "development",
 
-  mode: "development",
+    devtool: "inline-source-map",
 
-  devtool: "source-map",
+    devServer: {
+      open: true,
+      contentBase: `${__dirname}/dist`,
+      watchContentBase: true
+    },
 
-  entry: "./src/index.tsx",
+    entry: "./src/index.tsx",
 
-  output: {
-    path: `${__dirname}/dist`,
-    filename: "main.[hash].js"
-  },
+    output: {
+      path: `${__dirname}/dist`,
+      filename: "main.[hash].js"
+    },
 
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"],
-    alias: {
-      "~/src": `${__dirname}/src/`
-    }
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.ts(x?)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: "ts-loader"
-          }
-        ]
-      },
-      {
-        enforce: "pre",
-        test: /\.js$/,
-        loader: "source-map-loader"
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"],
+      alias: {
+        "~/src": `${__dirname}/src/`
       }
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.ts(x?)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: "ts-loader"
+            }
+          ]
+        },
+        {
+          enforce: "pre",
+          test: /\.js$/,
+          loader: "source-map-loader"
+        }
+      ]
+    },
+
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: `${__dirname}/public/index.html`
+      })
     ]
-  },
+  };
 
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: `${__dirname}/public/index.html`
-    })
-  ],
-
-  devServer: {
-    open: true,
-    contentBase: `${__dirname}/dist`,
-    watchContentBase: true
+  if (options.mode === "production") {
+    setting.mode = "production";
+    setting.devtool = false;
+    setting.devServer = {};
   }
-}
+
+  return setting;
+};
